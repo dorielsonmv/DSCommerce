00dbd9248847f024dce94122fa7b0cf7b8afef87
Dorielson Martins <dorielson.mv@gmail.com>
2025-07-31 01:10:05 -0300

    POST new product

--- DSCommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ DSCommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -19,7 +19,6 @@ public class ProductService {
 
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
-
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
     }
@@ -29,4 +28,18 @@ public class ProductService {
         Page<Product> result = repository.findAll(pageable);
         return result.map(x->new ProductDTO(x));
     }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+
+        Product entity = new Product();
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription() );
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
+    }
+
 }
